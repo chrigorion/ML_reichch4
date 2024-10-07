126e9af4c4e730091f983b6ee3de553730016199
chrigorion <reichch4@students.zhaw.ch>
2024-10-07 21:16:23 +0200

    changes

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# CSV-Datei laden
-df = pd.read_csv("Mental Health Dataset.csv")
-
-# Ersten Überblick verschaffen
-print(df.head())
-print(df.info())
-print(df.describe(include="all"))
